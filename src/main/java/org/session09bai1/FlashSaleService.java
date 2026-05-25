c5bebdae50c054cc09845e3fb468b4aa593001c1
Quoc Binh <binhtrieuhn99@gmail.com>
2026-05-25 15:20:02 +0700

    fix code

--- src/main/java/org/session09bai1/FlashSaleService.java
+++ src/main/java/org/session09bai1/FlashSaleService.java
@@ -5,34 +5,17 @@ import org.slf4j.LoggerFactory;
 
 public class FlashSaleService {
 
-    private static final Logger logger =
-            LoggerFactory.getLogger(FlashSaleService.class);
+    private static final Logger logger = LoggerFactory.getLogger(FlashSaleService.class);
 
     public void applyDiscount(String userId, String code) {
 
-        logger.info(
-                "Đang xử lý mã: {} cho user: {}",
-                code,
-                userId
-        );
+        logger.info("Đang xử lý mã: {} cho user: {}", code, userId);
 
         try {
-
             // ... logic xử lý
-
-            logger.info(
-                    "Áp dụng mã giảm giá thành công cho user: {}",
-                    userId
-            );
-
+            logger.info("Áp dụng mã giảm giá thành công cho user: {}", userId);
         } catch (Exception e) {
-
-            logger.error(
-                    "Lỗi khi áp dụng mã giảm giá cho user: {} với code: {}",
-                    userId,
-                    code,
-                    e
-            );
+            logger.error("Lỗi khi áp dụng mã giảm giá cho user: {} với code: {}", userId, code, e);
         }
     }
 }
